fa080bd13476bce4c156ca7664e259c6831b5580
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2023-06-27 21:23:34 +0530

    params-query-postman

--- 3-express-basics/server.js
+++ 3-express-basics/server.js
@@ -13,6 +13,58 @@ app.get("/users", (req, res) => {
   res.status(200).json({ users });
 });
 
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map(({ id, title, description, image }) => {
+    return { id, title, description, image };
+  });
+  res.status(200).json(newProducts);
+});
+
+// params
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res
+      .status(404)
+      .json({ response: false, message: "Product not found" });
+  }
+  res.status(200).json(singleProduct);
+});
+
+app.get("/api/products/:productID/review/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.status(200).json({ message: "test" });
+});
+
+// query
+app.get("/api/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.title.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({
+      response: "success",
+      message: "No products matches the condition",
+    });
+  }
+  res.status(200).json(sortedProducts);
+});
+
 app.all("*", (req, res) => {
   res.status(404).send("<h1>Something went wrong</h1>");
 });
